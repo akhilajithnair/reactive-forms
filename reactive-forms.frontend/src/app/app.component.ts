c2d1134cf779ddb1f8ace3e4bf222a185ca7c401
Akhil Nair <Akhil.Nair@avl.com>
2022-06-01 14:53:29 +0530

    add forms and formsgroup

--- reactive-forms.frontend/src/app/app.component.ts
+++ reactive-forms.frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms.frontend';
+  myForm: FormGroup;
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      message: new FormControl('')
+    });
+  }
+
+  onSubmit(myForm: FormGroup) {
+    console.log('form valid: ', myForm.valid);
+    console.log('name value: ', myForm.value.name);
+    console.log('email value: ', myForm.value.email);
+    console.log('message value: ', myForm.value.message);
+
+  }
 }
